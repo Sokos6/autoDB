95efe117da80efd62088a3548eb71ecd76abd48a
Will Sokolowski <will.sokolowski@gmail.com>
2020-05-18 16:49:33 -0500

    updated mac

---
+++ src/main/java/com/motus/mcardb/McardbApplication.java
@@ -0,0 +1,13 @@
+package com.motus.mcardb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McardbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McardbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motus/mcardb/McardbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motus.mcardb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McardbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
