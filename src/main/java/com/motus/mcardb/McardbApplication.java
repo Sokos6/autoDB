d7c9cf1537cf3ec59a8f4edbf3d04fdbdd3632bb
Will Sokolowski <will.sokolowski@gmail.com>
2020-05-20 15:04:47 -0500

    command line runner

--- src/main/java/com/motus/mcardb/McardbApplication.java
+++ src/main/java/com/motus/mcardb/McardbApplication.java
@@ -1,7 +1,9 @@
 package com.motus.mcardb;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class McardbApplication {
@@ -9,5 +11,15 @@ public class McardbApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(McardbApplication.class, args);
 	}
+	
+	@Bean
+	
+	CommandLineRunner runner() {
+		return args -> {
+			// Code here
+		};
+	}
+	
+	
 
 }
