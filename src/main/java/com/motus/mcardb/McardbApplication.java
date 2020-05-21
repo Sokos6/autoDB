ff0b7b25d9b362355db1bedaca65507b7c3bc41f
Will Sokolowski <will.sokolowski@gmail.com>
2020-05-21 14:53:52 -0500

    pom and friends

--- src/main/java/com/motus/mcardb/McardbApplication.java
+++ src/main/java/com/motus/mcardb/McardbApplication.java
@@ -8,31 +8,43 @@ import org.springframework.context.annotation.Bean;
 
 import com.motus.mcardb.domain.Car;
 import com.motus.mcardb.domain.CarRepository;
+import com.motus.mcardb.domain.Owner;
+import com.motus.mcardb.domain.OwnerRepository;
 
 @SpringBootApplication
 public class McardbApplication {
-	
+	// Inject repositories
 	@Autowired
 	private CarRepository repository;
+
+	@Autowired
+	private OwnerRepository orepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(McardbApplication.class, args);
 	}
-	
+
 	@Bean
 	
 	CommandLineRunner runner(){
         return args -> {
-          // Save demo data to database
-          repository.save(new Car("Ford", "Mustang", "Red",
-           "ADF-1121", 2017, 59000));
-          repository.save(new Car("Nissan", "Leaf", "White",
-           "SSJ-3002", 2014, 29000));
-          repository.save(new Car("Toyota", "Prius", "Silver",
-           "KKO-0212", 2018, 39000));
+        	// add owner objects and save these to db
+        	Owner owner1 = new Owner("Will", "Sokolowski");
+        	Owner owner2 = new Owner("Oscar", "Millan");
+        	orepository.save(owner1);
+        	orepository.save(owner2);
+        	
+        	// Add car object with link to owners and save these to db 
+        	Car car = new Car("Ford", "Mustang", "Red", 
+                    "ADF-1121", 2017, 59000, owner1);
+                repository.save(car);
+                car = new Car("Nissan", "Leaf", "White",
+                    "SSJ-3002", 2014, 29000, owner2);
+                repository.save(car);
+                car = new Car("Toyota", "Prius", "Silver",
+                    "KKO-0212", 2018, 39000, owner2);
+                repository.save(car);
         };
 	}
-	
-	
 
 }
--- src/main/java/com/motus/mcardb/domain/Car.java
+++ src/main/java/com/motus/mcardb/domain/Car.java
@@ -23,7 +23,7 @@ public class Car {
 	
 	public Car() {}
 	
-	public Car(String brand, String model, String color, String registerNumber, int year, int price) {
+	public Car(String brand, String model, String color, String registerNumber, int year, int price, Owner owner) {
 		super();
 		this.brand = brand;
 		this.model = model;
@@ -31,6 +31,7 @@ public class Car {
 		this.registerNumber = registerNumber;
 		this.year = year;
 		this.price = price;
+		this.owner = owner;
 	}
 	
 	public Owner getOwner() {
