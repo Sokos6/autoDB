42c1b9f974eb0093afcd31e4ef1d5e9e88b6c17b
Will Sokolowski <will.sokolowski@gmail.com>
2020-05-22 19:06:35 -0500

    config method changes post method and filter

--- src/main/java/com/motus/mcardb/SecurityConfig.java
+++ src/main/java/com/motus/mcardb/SecurityConfig.java
@@ -1,12 +1,20 @@
 package com.motus.mcardb;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.motus.mcardb.service.UserDetailServiceImpl;
 
@@ -15,6 +23,22 @@ import com.motus.mcardb.service.UserDetailServiceImpl;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Autowired
   private UserDetailServiceImpl userDetailsService; 
+  
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+	 http.csrf().disable().cors().and().authorizeRequests()
+	  .antMatchers(HttpMethod.POST, "/login").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        // Filter for the api/login requests
+        .addFilterBefore(new LoginFilter("/login", authenticationManager()),
+                UsernamePasswordAuthenticationFilter.class)
+        // Filter for other requests to check JWT in header
+        .addFilterBefore(new AuthenticationFilter(),
+                UsernamePasswordAuthenticationFilter.class);
+  }
+  
+  
 
   @Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
