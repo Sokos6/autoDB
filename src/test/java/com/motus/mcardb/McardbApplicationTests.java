0a2c9ceb260bfe39fdadb5c982f8c027090e3de4
Will Sokolowski <will.sokolowski@gmail.com>
2020-05-22 20:01:32 -0500

    passing junit

--- src/test/java/com/motus/mcardb/McardbApplicationTests.java
+++ src/test/java/com/motus/mcardb/McardbApplicationTests.java
@@ -1,13 +1,24 @@
 package com.motus.mcardb;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.motus.mcardb.web.CarController;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class McardbApplicationTests {
+public class McardbApplicationTests {
+  @Autowired
+  private CarController controller;
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  public void contextLoads() {
+    assertThat(controller).isNotNull();
+  }
 
 }
