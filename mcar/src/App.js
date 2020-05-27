f9f0cf7e15d1a578646bf143b616a8e98ce24faf
Will Sokolowski <will.sokolowski@gmail.com>
2020-05-27 16:34:57 -0500

    material and app.js set up

--- mcar/src/App.js
+++ mcar/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <AppBar position="static" color="blue">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            CarList
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
