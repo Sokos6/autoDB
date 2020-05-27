aa07e60dbbb1190e209c7898540872612d4f02ad
Will Sokolowski <will.sokolowski@gmail.com>
2020-05-27 17:45:58 -0500

    fetchCars

--- mcar/src/components/CarList.js
+++ mcar/src/components/CarList.js
@@ -3,7 +3,6 @@ import { SERVER_URL } from '../constants';
 import ReactTable from 'react-table-v6';
 import 'react-table-v6/react-table.css';
 
-
 class CarList extends Component {
   constructor(props) {
     super(props);
@@ -11,6 +10,10 @@ class CarList extends Component {
   }
 
   componentDidMount() {
+    this.fetchCars();
+  }
+
+  fetchCars = () => {
     fetch(SERVER_URL + 'api/cars')
       .then((response) => response.json())
       .then((responseData) => {
@@ -21,29 +24,56 @@ class CarList extends Component {
       .catch((err) => console.error(err));
   }
 
+
+
   render() {
     // const { cars } = this.state;
-    const columns = [{
-      Header: 'Brand',
-      accessor: 'brand'
-    }, {
-      Header: 'Model',
-      accessor: 'model',
-    }, {
-      Header: 'Color',
-      accessor: 'color',
-    }, {
-      Header: 'Year',
-      accessor: 'year',
-    }, {
-      Header: 'Price €',
-      accessor: 'price',
-    },]
+    const columns = [
+      {
+        Header: 'Brand',
+        accessor: 'brand',
+      },
+      {
+        Header: 'Model',
+        accessor: 'model',
+      },
+      {
+        Header: 'Color',
+        accessor: 'color',
+      },
+      {
+        Header: 'Year',
+        accessor: 'year',
+      },
+      {
+        Header: 'Price $',
+        accessor: 'price',
+      },
+      {
+        id: 'deletebutton',
+        sortable: false,
+        filterable: false,
+        width: 100,
+        accessor: '_links.self.href',
+        Cell: ({ value }) => (
+          <button
+            onClick={() => {
+              this.onDelClick(value);
+            }}
+          >
+            Delete
+          </button>
+        ),
+      },
+    ];
 
     return (
       <div className='car-list'>
-        <ReactTable data={this.state.cars} columns={columns}
-        filterable={true}/>
+        <ReactTable
+          data={this.state.cars}
+          columns={columns}
+          filterable={true}
+        />
       </div>
     );
   }
